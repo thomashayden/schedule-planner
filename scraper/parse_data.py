1e6ca11c6d0498b8f2d05f06b5c691181465cd81
thomashayden <hayden.t@husky.neu.edu>
2017-11-16 21:25:37 -0500

    Rework detection system.

--- scraper/parse_data.py
+++ scraper/parse_data.py
@@ -32,11 +32,36 @@ def pull_data_from_body(tr):
             del data[i]
     while '' in data: # Loop through and remove any completely empty lines
         data.remove('')
-    dict_of_info = {}
-    for line_number in range(0,len(data)-1):
-        dict_of_info = merge_two_dicts(dict_of_info, interpret_data_from_body(line_number, data[line_number])) 
-    print(len(dict_of_info)) # Print how many lines of data there is. Only for debugging purposes
-    print(dict_of_info) # Also print the data. Also for debugging
+    # These are the markings for where information is. By default everything is -1, which means it isn't there.
+    dict_of_info = { assc_term_start : -1,
+                     assc_term_end : -1,
+                     lvl_start : -1,
+                     lvl_end : -1,
+                     attr_start : -1,
+                     attr_end : -1,
+                     instr_start : -1,
+                     instr_end : -1,
+                     sched_type_start : -1,
+                     sched_type_end : -1,
+                     instr_method_start : -1,
+                     instr_method_end : -1,
+                     restr_start : -1,
+                     restr_end : -1,
+                     preq_start : -1,
+                     preq_end : -1,
+                     sched_start : -1,
+                     sched_end : -1
+                     }
+    dict_of_info[assc_term_start] = "TEMP"
+    print(len(data)) # Print how many lines of data there is. Only for debugging purposes
+    print(data) # Also print the data. Also for debugging
+
+# Returns the index of the first found string containing the given string
+def search_for_string(str, arr):
+    for i in range(0, len(arr)-1):
+        if arr[i].contains(str):
+            return i
+    return -1 # Returns -1 if the string is not found
 
 def interpret_data_from_body(num,value):
     if num is 0:
@@ -48,6 +73,17 @@ def interpret_data_from_body(num,value):
     elif num is 2:
         # Always attributes
         return {value.split(":")[0] : value.split(":").pop(0)}
+    elif num is 3:
+        # Always Instructor(s) (I've only seen a single instructor, so who knows what happens when there are more than one...
+        return {value.split(":")[0] : value.split(":").pop(0)}
+    elif num is 4:
+        # Schedule type
+        return {value.split(":")[0] : value.split(":").pop(0)}
+    elif num is 5:
+        # Instructional method? Just a single value
+        return {"Instructional Method" : value}
+    elif num is 6:
+        print("TEMP")
     else:
         # Anything else at the moment
         tmp = {}
